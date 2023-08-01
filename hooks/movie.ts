b1e886679051f5e8c5c9d0cab38c3d2db8dabca0
kunal00000 <kunalverma2468@gmail.com>
2023-08-01 14:12:58 +0530

    refactor: env name

--- hooks/movie.ts
+++ hooks/movie.ts
@@ -15,7 +15,7 @@ async function fetchMovie() {
       method: "GET",
       headers: {
         accept: "application/json",
-        Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_READ_ACCESS_TOKEN}`
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_TMDB_API_READ_ACCESS_TOKEN}`
       }
     });
     const data = await response.json();
